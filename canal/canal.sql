c9afbff62c0b007ca895db4f37ecdc56b9604669
xiaowu <943915349@qq.com>
2023-03-02 17:11:47 +0800

    update

--- canal/canal.sql
+++ canal/canal.sql
@@ -17,13 +17,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_adapter_config`;
 CREATE TABLE `canal_adapter_config` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `content` text NOT NULL,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+                                        `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                                        `category` varchar(45) NOT NULL,
+                                        `name` varchar(45) NOT NULL,
+                                        `status` varchar(45) DEFAULT NULL,
+                                        `content` text NOT NULL,
+                                        `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -31,11 +31,11 @@ CREATE TABLE `canal_adapter_config` (
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_cluster`;
 CREATE TABLE `canal_cluster` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(63) NOT NULL,
-  `zk_hosts` varchar(255) NOT NULL,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+                                 `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(63) NOT NULL,
+                                 `zk_hosts` varchar(255) NOT NULL,
+                                 `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -43,16 +43,16 @@ CREATE TABLE `canal_cluster` (
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_config`;
 CREATE TABLE `canal_config` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `cluster_id` bigint(20) DEFAULT NULL,
-  `server_id` bigint(20) DEFAULT NULL,
-  `name` varchar(45) NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `content` text NOT NULL,
-  `content_md5` varchar(128) NOT NULL,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `sid_UNIQUE` (`server_id`)
+                                `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                                `cluster_id` bigint(20) DEFAULT NULL,
+                                `server_id` bigint(20) DEFAULT NULL,
+                                `name` varchar(45) NOT NULL,
+                                `status` varchar(45) DEFAULT NULL,
+                                `content` text NOT NULL,
+                                `content_md5` varchar(128) NOT NULL,
+                                `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                PRIMARY KEY (`id`),
+                                UNIQUE KEY `sid_UNIQUE` (`server_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -60,16 +60,16 @@ CREATE TABLE `canal_config` (
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_instance_config`;
 CREATE TABLE `canal_instance_config` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `cluster_id` bigint(20) DEFAULT NULL,
-  `server_id` bigint(20) DEFAULT NULL,
-  `name` varchar(45) NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `content` text NOT NULL,
-  `content_md5` varchar(128) DEFAULT NULL,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
+                                         `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                                         `cluster_id` bigint(20) DEFAULT NULL,
+                                         `server_id` bigint(20) DEFAULT NULL,
+                                         `name` varchar(45) NOT NULL,
+                                         `status` varchar(45) DEFAULT NULL,
+                                         `content` text NOT NULL,
+                                         `content_md5` varchar(128) DEFAULT NULL,
+                                         `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                         PRIMARY KEY (`id`),
+                                         UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -77,16 +77,16 @@ CREATE TABLE `canal_instance_config` (
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_node_server`;
 CREATE TABLE `canal_node_server` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `cluster_id` bigint(20) DEFAULT NULL,
-  `name` varchar(63) NOT NULL,
-  `ip` varchar(63) NOT NULL,
-  `admin_port` int(11) DEFAULT NULL,
-  `tcp_port` int(11) DEFAULT NULL,
-  `metric_port` int(11) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+                                     `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                                     `cluster_id` bigint(20) DEFAULT NULL,
+                                     `name` varchar(63) NOT NULL,
+                                     `ip` varchar(63) NOT NULL,
+                                     `admin_port` int(11) DEFAULT NULL,
+                                     `tcp_port` int(11) DEFAULT NULL,
+                                     `metric_port` int(11) DEFAULT NULL,
+                                     `status` varchar(45) DEFAULT NULL,
+                                     `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -94,15 +94,15 @@ CREATE TABLE `canal_node_server` (
 -- ----------------------------
 DROP TABLE IF EXISTS `canal_user`;
 CREATE TABLE `canal_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(31) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `name` varchar(31) NOT NULL,
-  `roles` varchar(31) NOT NULL,
-  `introduction` varchar(255) DEFAULT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+                              `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                              `username` varchar(31) NOT NULL,
+                              `password` varchar(128) NOT NULL,
+                              `name` varchar(31) NOT NULL,
+                              `roles` varchar(31) NOT NULL,
+                              `introduction` varchar(255) DEFAULT NULL,
+                              `avatar` varchar(255) DEFAULT NULL,
+                              `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                              PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
